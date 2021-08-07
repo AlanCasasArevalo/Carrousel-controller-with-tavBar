bc681c7912f5860edf6defc644c258a7aab0beab
Alan Casas <alancasasarevalo@gmail.com>
2021-08-07 08:47:01 +0200

    Initial Commit

---
+++ Carrousel controller with tavBar/Carrousel_controller_with_tavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carrousel_controller_with_tavBarApp.swift
+//  Carrousel controller with tavBar
+//
+//  Created by Alan Casas on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Carrousel_controller_with_tavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
